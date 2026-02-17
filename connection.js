b86db7f36919c1cb4b5493e4a3cfdc038de8f0c2
Allen Ernest <allenernest360@gmail.com>
2026-02-17 08:18:28 +0300

    Replaced mysql with mysql2

--- connection.js
+++ connection.js
@@ -1,17 +1,18 @@
-import mysql from 'mysql'
+import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'root123',
     database: 'allen_api'
-})
+});
 
 connection.connect((err) => {
     if (err) {
-        console.error('Error connecting to the database:', err)
-        return
+        console.error('Error connecting to the database:', err.message);
+        return;
     }
-})
+    console.log('Connected to MySQL database');
+});
 
-export default connection
+export default connection;
